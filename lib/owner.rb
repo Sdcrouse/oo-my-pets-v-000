4316ed6289400d130fc29f9d20ba287cfa1cdd5a
Steven Crouse <sdcrouse91@gmail.com>
2018-09-07 08:35:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,4 +43,12 @@ class Owner
     self.pets[:dogs].each {|dog| dog.mood = "happy"}
   end 
   
+  def play_with_cats 
+    self.pets[:cats].each {|cat| cat.mood = "happy"}
+  end 
+  
+  def feed_fish
+    self.pets[:fish].each {|fish| fish.mood = "happy"}
+  end 
+  
 end
